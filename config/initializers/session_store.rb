e01633313d2a19d53a09cd60de9beb06cb011224
vlppz <papazovvladimir88@gmail.com>
2026-06-03 23:14:20 +0300

    some changes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,9 @@
+secure_session_cookie = if ENV.key?("SESSION_COOKIE_SECURE")
+  ActiveModel::Type::Boolean.new.cast(ENV["SESSION_COOKIE_SECURE"])
+else
+  Rails.env.production?
+end
+
 Rails.application.config.session_store :redis_session_store,
                                        key: "_tickets_system_session",
                                        redis: {
@@ -5,6 +11,6 @@ Rails.application.config.session_store :redis_session_store,
                                          expire_after: 1.year,
                                          key_prefix: "session:"
                                        },
-                                       secure: Rails.env.production?,
+                                       secure: secure_session_cookie,
                                        httponly: true,
                                        same_site: :lax
